5d550fe41ec38c0dca12093555bfe0412a591601
Odiyach <odiyach0910@gmail.com>
2024-09-17 14:40:21 +0200

    virement/debit en booleen

---
+++ src/Compte.java
@@ -0,0 +1,165 @@
+public class Compte
+{
+    private int numCompte;
+    private Personne titulaire;
+    private double soldeCompte;
+    private double decouvertMaxAutorise;
+    private double debitMaxAutorise;
+
+    private static final int decouvertMaxiAutorise = 800;
+
+    //constructeur permettant de créer un compte avec seulement un numéro de compte et le titulaire.
+    public Compte(int numCompte, Personne titulaire)
+    {
+        this.numCompte = numCompte;
+        this.titulaire = titulaire;
+        this.soldeCompte=0;
+        this.decouvertMaxAutorise=800;
+        this.debitMaxAutorise=1000;
+    }
+
+    //constructeur permettant de définir à la création du compte, à part le numéro de compte et le titulaire, de nouvelles caractéristiques.
+    public Compte(int numCompte, Personne titulaire, double soldeCompte, double decouvertMaxAutorise, double debitMaxAutorise)
+    {
+        this.numCompte = numCompte;
+        this.titulaire = titulaire;
+        this.soldeCompte = soldeCompte;
+        this.decouvertMaxAutorise = decouvertMaxAutorise;
+        this.debitMaxAutorise = debitMaxAutorise;
+    }
+
+
+
+    public int getNumCompte()
+    {
+        return numCompte;
+    }
+
+    //la méthode getTitulaire permet de recupérer l'objet titulaire lié au compte.
+    public Personne getTitulaire() {
+        return titulaire;
+    }
+
+    public double getSoldeCompte() {
+        return soldeCompte;
+    }
+    public double getDecouvertMaxAutorise() {
+        return decouvertMaxAutorise;
+    }
+    public double getDebitMaxAutorise() {
+        return debitMaxAutorise;
+    }
+    public void setDecouvertMaxAutorise(double decouvertMaxAutorise)
+    {
+        this.decouvertMaxAutorise = decouvertMaxAutorise;
+    }
+    public void setDebitMaxAutorise(double debitMaxAutorise) {
+        this.debitMaxAutorise = debitMaxAutorise;
+    }
+
+
+
+    //la méthode getDecouvertDuCompte permet de vérifier si le compte est à découvert.
+    //si le solde est positif ou null, le découvert sera de 0, sinon le découvert sera la valeur absolu du solde.
+    public double getDecouvertDuCompte()
+    {
+        if(soldeCompte>=0)
+        {
+            return 0;
+        }
+        return soldeCompte * -1;
+    }
+
+    public String messageDecouvert()
+    {
+        String message="";
+        if (getDecouvertDuCompte()>0)
+        {
+            message="Le compte est à découvert de: "+ getDecouvertDuCompte();
+
+        }
+        return message;
+    }
+
+
+
+
+    //montantDebitAutorise est une methode qui calcule le montant de débit maximum autorisé.
+    public double montantDebitAutorise()
+    {
+        double TotalPossible= this.soldeCompte + this.decouvertMaxAutorise;
+
+        if( TotalPossible > debitMaxAutorise)
+        {
+            return  debitMaxAutorise;
+        }
+        else
+        {
+            return  TotalPossible;
+
+        }
+    }
+
+
+
+    // la methode crediterCompte permet d'ajouter un montant positif au solde
+    public void crediterCompte(double montant)
+    {
+
+        this.soldeCompte+=montant;
+
+    }
+
+
+
+    //la methode debiterCompte permet de retirer de l'argent.
+    //Pour cela, il vérifie d'abord si le montant a retirer dépasse la limite du débit autorisé.
+    //Dans ce cas le débit est refusé, sinon, le débit est effectué.
+    public boolean debiterCompte(double montant)
+    {
+
+        if (montant> montantDebitAutorise())
+        {
+            return  false;
+        }
+        else
+        {
+            this.soldeCompte-=montant;
+            return  true;
+        }
+
+    }
+
+    //effectuerVirement est une mthode qui permet d'effectuer un virement vers un commpte beneficiaire.
+    // cela entrainera le debit du compte crediteur et le credit du compte bénéficiaire.
+    public boolean effectuerVirement( Compte beneficiaire, double montant)
+    {
+        if(montant<= montantDebitAutorise())
+        {
+            this.debiterCompte(montant);
+            beneficiaire.crediterCompte(montant);
+            return true;
+        }
+        else
+        {
+            return false;
+
+
+        }
+    }
+
+
+    public String CaracteristiquesCompte()
+    {
+        return "----Informations----"+"\n"+
+                this.getNumCompte()+" : "+this.getTitulaire().getNom()+" | "+
+                "Découvert maximal autorisé : "+this.getDecouvertMaxAutorise()+" "+
+                "Débit maximal autorisé : "+this.getDebitMaxAutorise()+" "+
+                "Solde du compte : "+this.getSoldeCompte()+" "+
+                this.messageDecouvert()+"\n";
+
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Personne p1 =new Personne("J. DUPONT ");
+        Compte c1 =new Compte(1,p1);
+        c1.crediterCompte(1000);
+
+        Personne p2 =new Personne("C. DURANT  ");
+        Compte c2 =new Compte(2,p2,50000,5000,6000);
+
+        System.out.println(c1.CaracteristiquesCompte()+c2.CaracteristiquesCompte());
+
+
+        c1.debiterCompte(300);
+        System.out.println("c1 apres debit de 300 : Solde => "+c1.getSoldeCompte());
+        c2.debiterCompte(600);
+        System.out.println("c2 apres debit de 600 : Solde => "+c2.getSoldeCompte());
+        c1.crediterCompte(500);
+        System.out.println("c1 apres credit de 500: Solde => "+c1.getSoldeCompte());
+
+        System.out.println(c1.CaracteristiquesCompte()+c2.CaracteristiquesCompte());
+
+
+        c2.effectuerVirement(c1,1000);
+
+
+        System.out.println("c1  et c2 après virement de 1000 de c2 vers c1: "+"\n"+c1.CaracteristiquesCompte()+c2.CaracteristiquesCompte());
+
+
+        //Tests pertinents:
+
+        // essai d'un debit supérieur au débit max autorisé (tester l'erreur)
+        c1.debiterCompte(1100);
+
+        //réatribution du débit max a 1200
+        c1.setDebitMaxAutorise(1200);
+
+        //verification du montant de débit max du compte c1
+        c1.montantDebitAutorise();
+
+        // 2ème essai d'un debit maintenant inférieur au débit max autorisé
+        c1.debiterCompte(1100);
+
+        //caracteristiques du compte c1 apres les différents testsq
+        System.out.println(c1.CaracteristiquesCompte());
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
