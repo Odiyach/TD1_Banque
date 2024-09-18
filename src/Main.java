41fbdde1a1d3ff3f544272267901c0cd437a8888
yoelchicheportiche <yoelchiche3112@gmail.com>
2024-09-19 00:14:48 +0200

    main try

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+
+
     public static void main(String[] args) throws Exception {
 
 
@@ -12,27 +14,37 @@ public class Main {
 
 
 
-        /*
+
+
         try {
-            c1.crediterCompte(500);
+            c1.crediterCompte(-500);
+        } catch (CompteException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            c1.crediterCompte(1500);
+            System.out.println(c1.getSoldeCompte());
+        } catch (CompteException e) {
+            System.out.println(e.getMessage());
+
+        }
+
+        try {
+            c1.debiterCompte(200);
+            System.out.println(c1.getSoldeCompte());
+
         } catch (CompteException e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            c1.debiterCompte(2000);
-        } catch (CompteException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            c1.effectuerVirement(c2, 1000);
+            c1.effectuerVirement(c2, 10000);
         } catch (CompteException e) {
             System.out.println(e.getMessage());
         }
 
 
-         */
+
 
 
     }
